6f58496f9aa12994c1a6b346b82f8eb09c8fcd1a
DvTanviR <tanvir99ww0@gmail.com>
2025-09-25 17:35:03 +0700

    push

--- app/settings.py
+++ app/settings.py
@@ -86,12 +86,20 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG == False:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR/ 'database/db.sqlite3',
+        }
+    }
 
 
 # Password validation
